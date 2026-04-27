1b939b00f596001ef706476956064f7c8a861025
EduardM173 <eduardo.apaza@ucb.edu.bo>
2026-04-27 15:38:21 -0400

    error de prueba para jenkins

--- frontend/src/context/CartContext.jsx
+++ frontend/src/context/CartContext.jsx
@@ -9,7 +9,7 @@ export const CartProvider = ({ children }) => {
     try {
       const saved = localStorage.getItem("cart_items");
       const parsed = saved ? JSON.parse(saved) : [];
-      return Array.isArray(parsed) ? parsed : [];
+      return Array.isArray(parsed) ? 
     } catch {
       return [];
     }
